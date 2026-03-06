5425e94ba0d78485adb3e7738ecf6668cb0b7713
Shubham Goyal <gshubham.05@gmail.com>
2026-03-06 12:54:50 +0530

    gtag

--- src/app/Components/GoogleAnalytics.js
+++ src/app/Components/GoogleAnalytics.js
@@ -2,7 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
-import * as gtag from "@/lib/gtag";
+import * as gtag from "@/app/lib/gtag";
 
 export default function GoogleAnalytics() {
   const pathname = usePathname();
